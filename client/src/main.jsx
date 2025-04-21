238e0cf83b779ea71df5288eb1aa97885cbba633
Darshita Patel <64008667+Darshita-04@users.noreply.github.com>
2025-04-21 18:08:47 -0400

    Update main.jsx
    Removed BrowserRouter from main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store.js";
 import App from "./App.jsx";
@@ -8,10 +7,8 @@ import "./styles/main.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-    </BrowserRouter>
   </StrictMode>
 );

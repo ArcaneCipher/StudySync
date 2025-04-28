3dd24158a5d108023c4171686e01b75cb78740d2
ArcaneCipher <desjardins.jarp@gmail.com>
2025-04-28 01:55:14 -0400

    Fix: Goals table now actually links to a real deck id

--- server/db/migrate/20250428052243_add_deck_id_to_goals.rb
+++ server/db/migrate/20250428052243_add_deck_id_to_goals.rb
@@ -1,6 +1,5 @@
 class AddDeckIdToGoals < ActiveRecord::Migration[7.1]
   def change
-    add_column :goals, :deck_id, :integer
-    add_foreign_key :goals, :decks
+    add_reference :goals, :deck, null: false, foreign_key: true
   end
 end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -113,7 +113,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_28_052243) do
     t.boolean "is_public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "deck_id"
+    t.bigint "deck_id", null: false
+    t.index ["deck_id"], name: "index_goals_on_deck_id"
     t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
